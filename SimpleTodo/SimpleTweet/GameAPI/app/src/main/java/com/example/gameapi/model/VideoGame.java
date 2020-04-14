d4787cc309d8451dfb9ab3118c039b90dccc83b2
Devonne <devonne.childs@gmail.com>
2020-04-14 14:49:46 -0400

    Test

---
+++ SimpleTodo/SimpleTweet/GameAPI/app/src/main/java/com/example/gameapi/model/VideoGame.java
@@ -0,0 +1,53 @@
+package com.example.gameapi.model;
+
+import android.widget.RatingBar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoGame {
+    String background_image;
+    String name;
+    double rating;
+    String next;
+    URL url;
+
+    public VideoGame(JSONObject jsonObject) throws JSONException {
+        name = jsonObject.getString("name");
+        background_image = jsonObject.getString("background_image");
+        rating = jsonObject.getDouble("rating");
+
+        //description = jsonObject.getString("description");
+    }
+
+    public static List<VideoGame> fromJsonArray(JSONArray videogameJsonArray) throws JSONException {
+        List<VideoGame> videoGames = new ArrayList<>();
+        for(int i=0; i<videogameJsonArray.length(); i++)
+        {
+            videoGames.add(new VideoGame(videogameJsonArray.getJSONObject(i)));
+        }
+        return videoGames;
+    }
+
+    public String getBackground_image() {
+        return background_image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+    public String getNext() {return next;}
+
+   /* public String getDescription() {
+        return description;
+    }*/
+}
---
+++ VGR-APP-master/app/src/main/java/com/example/GameRecommender/adapters/VideoGameAdapter.java
@@ -0,0 +1,69 @@
+package com.example.GameRecommender.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.GameRecommender.R;
+import com.example.GameRecommender.models.VideoGame;
+
+import java.util.List;
+
+public class VideoGameAdapter extends RecyclerView.Adapter<VideoGameAdapter.ViewHolder>{
+
+    Context context;
+    List<VideoGame> videoGames;
+    RatingBar ratingBar;
+
+    public VideoGameAdapter(Context context, List<VideoGame> videoGames) {
+        this.context = context;
+        this.videoGames = videoGames;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View gameView = LayoutInflater.from(context).inflate(R.layout.item_game, parent, false);
+        return new ViewHolder(gameView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        VideoGame videoGame = videoGames.get(position);
+        holder.bind(videoGame);
+    }
+
+    @Override
+    public int getItemCount() {
+        return videoGames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView vgName;
+        TextView vgDescription;
+        ImageView ivPicture;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            vgName = itemView.findViewById(R.id.vgName);
+            ratingBar = itemView.findViewById(R.id.ratingBar);
+            ivPicture = itemView.findViewById(R.id.ivPicture);
+        }
+
+        public void bind(VideoGame videoGame) {
+            vgName.setText(videoGame.getName());
+            ratingBar.setRating((float) videoGame.getRating());
+            Glide.with(context).load(videoGame.getBackground_image()).into(ivPicture);
+
+        }
+    }
+}
